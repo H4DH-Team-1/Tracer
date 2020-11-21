1149fb55815e28050384563ad16d110b6c97cf7d
Andrew Newton <nootn@hotmail.com>
2020-11-21 11:38:34 +1000

    New console app for face id

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tracer
 Allows us to manage captured details
+
+
+## Prereqs
+
+Install dotnet core https://dotnet.microsoft.com/download
+
---
+++ identify-face/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace identify_face
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running");
+        }
+    }
+}
