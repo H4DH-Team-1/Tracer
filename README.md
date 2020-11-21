d39bec3a2766e67dd274b18494555cf62ba6f5ce
Andrew Newton <nootn@hotmail.com>
2020-11-21 13:04:04 +1000

    Got initial face API call working with dummy image

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # Tracer
-Allows us to manage captured details
+THIS IS A HACKATHON PROJECT - DO NOT USE AS PRODUCTION CODE!
+https://telstrahealth.com/H4DH - 2020, Team 1.
 
 
 ## Prereqs
 
 Install dotnet core https://dotnet.microsoft.com/download
+
+Set up an Azure subscription
+
+Create a Face resource in the Azure portal to get your key and endpoint (https://portal.azure.com/#create/Microsoft.CognitiveServicesFace)
+You can use the free pricing tier (F0) to try the service, and upgrade later to a paid tier for production.
+
+Put a key and endpoint from the Face resorce into Environment Variables:
+AZURE_COGNITIVE_SUBSCRIPTION_KEY
+AZURE_COGNITIVE_ENDPOINT
+
+Create an Azure storage account with the default options.
+Create a private container within there for "unknown-images" and set the URL for Environment Variable:
+AZURE_COGNITIVE_IMAGE_BASE_URL
 
--- identify-face/Program.cs
+++ identify-face/Program.cs
@@ -1,16 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 namespace identify_face
 {
   class Program
   {
-    static void Main(string[] args)
+    const string RECOGNITION_MODEL = RecognitionModel.Recognition03;
+    static async Task Main(string[] args)
     {
       Console.WriteLine("Running");
 
       var configCognitiveSubscriptionKey = Environment.GetEnvironmentVariable("AZURE_COGNITIVE_SUBSCRIPTION_KEY");
       var configCognitiveEndpoint = Environment.GetEnvironmentVariable("AZURE_COGNITIVE_ENDPOINT");
-      Console.WriteLine($"Got Endpoint: {configCognitiveEndpoint}");
+      var faceBaseImageUrl = Environment.GetEnvironmentVariable("AZURE_COGNITIVE_IMAGE_BASE_URL");
+
+      IFaceClient client = Authenticate(configCognitiveEndpoint, configCognitiveSubscriptionKey);
+
+      await DetectFaceExtract(client, faceBaseImageUrl, RECOGNITION_MODEL);
+    }
+
+    public static IFaceClient Authenticate(string endpoint, string key)
+    {
+      return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+    }
+
+    public static async Task DetectFaceExtract(IFaceClient client, string baseUrl, string recognitionModel)
+    {
+      //TODO: get all images from blob
+      var imageFileNames = new List<string>{$"{baseUrl}profile.jpg"};
+
+      foreach (var imageFileName in imageFileNames)
+      {
+        Console.WriteLine($"Calling: {client.Endpoint} for image {imageFileName}");
+        IList<DetectedFace> detectedFaces;
+
+        // Detect faces with all attributes from image url.
+        detectedFaces = await client.Face.DetectWithUrlAsync(imageFileName,
+          returnFaceAttributes: new List<FaceAttributeType?> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+          FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+          FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+          FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile },
+          // We specify detection model 1 because we are retrieving attributes.
+          detectionModel: DetectionModel.Detection01,
+          recognitionModel: recognitionModel);
+
+        Console.WriteLine($"{detectedFaces.Count} face(s) detected from image `{imageFileName}`.");
+      }
     }
   }
 }
